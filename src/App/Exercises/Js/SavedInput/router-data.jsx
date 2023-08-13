7720313f051c3c6b3e61fe2321094c0dd770242f
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-08-13 14:52:56 +0200

    Exercises JSON, XML and Storage

---
+++ src/App/Exercises/Js/SavedInput/router-data.jsx
@@ -0,0 +1,10 @@
+import { SavedInput } from './';
+
+export const blockRouterMetaData = {
+  path: 'saved-input',
+  date: '13-08-2023',
+  linkLabel: 'Saved Input',
+  blockNo: 33,
+  element: <SavedInput />,
+  tags: ['storage'],
+};
