8fcdadf208a5a741529177bda31aed27db24efa2
Nikolas Jadziński <nikolas.jadzinski@gmail.com>
2023-09-01 22:18:36 +0200

    Przykłady tworzenia testów

---
+++ src/App/Exercises/Js/Testing/router-data.jsx
@@ -0,0 +1,10 @@
+import { ToDoList } from './TestToDoList';
+
+export const blockRouterMetaData = {
+  path: 'testing-todo-list',
+  date: '01-09-2023',
+  linkLabel: 'Testing',
+  blockNo: 45,
+  element: <ToDoList />,
+  tags: ['components'],
+};
